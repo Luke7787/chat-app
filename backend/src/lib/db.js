8d2a15009ac525ad6423426db6750a2460280e7d
Luke Zhuang <Lukezhuang@yahoo.com>
2026-04-12 14:21:24 -0700

    finished mongodb connection file

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,3 +1,10 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {};
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection eroor", error);
+  }
+};
