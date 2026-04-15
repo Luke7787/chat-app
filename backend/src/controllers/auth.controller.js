e38a6c03d02758a91a2e57cc1d73c97fbad72981
Luke Zhuang <Lukezhuang@yahoo.com>
2026-04-15 15:43:52 -0700

    worked on auth.controller.js a bit more, still need to be completed tmr

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
@@ -25,6 +26,13 @@ export const signup = async (req, res) => {
 
     if (newUser) {
       // generate jwt token here
+      generateToken(newUser._id, res);
+
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+      });
     } else {
       res.status(400).json({ message: "Invalid user data" });
     }
