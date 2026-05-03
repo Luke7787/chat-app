7b07b50dcc6b3685a9e51a57cc574609c86d9a99
Luke Zhuang <Lukezhuang@yahoo.com>
2026-05-03 16:18:35 -0700

    finished getMessages function

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -26,5 +26,10 @@ export const getMessages = async (req, res) => {
         { senderId: userToChatId, receiverId: myId },
       ],
     });
-  } catch (error) {}
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
