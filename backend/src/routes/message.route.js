5d0e196e9badabdb61e66877b60a7dfa272466ab
Luke Zhuang <Lukezhuang@yahoo.com>
2026-05-02 17:47:14 -0700

    added getMessage import

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
