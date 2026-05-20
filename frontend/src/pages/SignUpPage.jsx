74af266810f977d43c9902d1574d35a004e5419d
Luke Zhuang <Lukezhuang@yahoo.com>
2026-05-19 22:24:33 -0700

    worked on frontend for signuppage

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
-import { MessageSquare } from "lucide-react";
+import { MessageSquare, User } from "lucide-react";
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -26,17 +26,37 @@ const SignUpPage = () => {
           {/* LOGO */}
           <div className="text-center mb-8">
             <div className="flex flex-col items-center gap-2 group">
-              <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
-                <MessageSquare className="size-6 text-primary" />
+              <div className="relative w-12 h-12 flex items-center justify-center">
+                <div className="absolute inset-0 bg-primary opacity-10 rounded-xl group-hover:opacity-20 transition-opacity"></div>
+                <MessageSquare className="w-6 h-6 text-primary relative z-10" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Create Account</h1>
-              <p className="text-base-content/60">
+              <p className="text-base-content opacity-60">
                 Get started with your free account
               </p>
             </div>
           </div>
-
-          <form onSubmit={handleSubmit} className="space-y-6"></form>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Full Name</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
+                  <User className="w-5 h-5 text-base-content opacity-40" />
+                </div>
+                <input
+                  type="text"
+                  className="input w-full pl-10 border border-solid border-gray-500 rounded-lg"
+                  placeholder="John Doe"
+                  value={formData.fullName}
+                  onChange={(e) =>
+                    setFormData({ ...formData, fullName: e.target.value })
+                  }
+                />
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
