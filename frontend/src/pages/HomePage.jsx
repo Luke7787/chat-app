c0d38d564fe3ac6fc16121dfa498feee1b444cff
Luke Zhuang <Lukezhuang@yahoo.com>
2026-06-29 10:10:43 -0700

    implemented NoChatSelected.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,6 @@
 import Sidebar from "../components/Sidebar";
+import NoChatSelected from "../components/NoChatSelected";
+import ChatContainer from "../components/ChatContainer";
 import { useChatStore } from "../store/useChatStore";
 
 const HomePage = () => {
