0574da98dbf5ad627473c853f1cb076a2a6ed426
Luke Zhuang <Lukezhuang@yahoo.com>
2026-07-04 11:55:50 -0700

    imported ChatHeader and MessageInput to ChatContainer.jsx

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useChatStore } from "../store/chatStore";
+import { useEffect } from "react";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
 
 const ChatContainer = () => {
-  return <div>ChatContainer</div>;
+  const { messages, getMessages, isMessageLoading, selectedUser } =
+    useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser?._id);
+  }, [selectedUser._id, getMessages]);
+
+  if (isMessageLoading) return <div>Loading...</div>;
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+
+      <p>messages...</p>
+
+      <MessageInput />
+    </div>
+  );
 };
 
 export default ChatContainer;
