36de82c3451c7591fbe9b75c6085e03610e2cfa1
Luke Zhuang <Lukezhuang@yahoo.com>
2026-07-05 14:06:29 -0700

    added const to MessageInput.jsx

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
+
 const MessageInput = () => {
+  const [text, setText] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
+  const fileInputRef = useRef(null);
+
   return <div>MessageInput</div>;
 };
-
 export default MessageInput;
