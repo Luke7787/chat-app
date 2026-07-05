daa6b17752e01c0b9a4a47767908df22ec8811b3
Luke Zhuang <Lukezhuang@yahoo.com>
2026-07-05 13:27:45 -0700

    added isMessageLoading Component

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -11,7 +11,14 @@ const ChatContainer = () => {
     getMessages(selectedUser?._id);
   }, [selectedUser._id, getMessages]);
 
-  if (isMessageLoading) return <div>Loading...</div>;
+  if (isMessageLoading)
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
 
   return (
     <div className="flex-1 flex flex-col overflow-auto">
