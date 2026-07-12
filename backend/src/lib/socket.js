4fed59b02ddf0f204dbfa21bc28062dadc93afa1
Luke Zhuang <Lukezhuang@yahoo.com>
2026-07-12 11:04:00 -0700

    worked on socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,4 +7,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, { cors: { origin: ["http://localhost:5173"] } });
 
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+  });
+});
+
 export { io, app, server };
