49ff4deadd1311effa72c6a851c5fd2561e28855
Luke Zhuang <Lukezhuang@yahoo.com>
2026-07-26 13:31:38 -0700

    change user to filterUsers

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -45,7 +45,7 @@ const Sidebar = () => {
         </div>
       </div>
       <div className="overflow-y-auto w-full py-3">
-        {users.map((user) => (
+        {filteredUsers.map((user) => (
           <button
             key={user._id}
             onClick={() => setSelectedUser(user)}
